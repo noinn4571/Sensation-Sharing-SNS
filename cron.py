18a74042924521e04aae519fc79f58b5acb465e0
noinn <saito.shin.o2@elms.hokudai.ac.jp>
2025-03-20 16:39:35 +0900

    'ベースのhtmlとcssを作りました'

--- cron.py
+++ cron.py
@@ -1,30 +1,42 @@
-import requests, json, datetime
+class get_oura_data:
+    
+    def get_lastbpm():
+        import requests, json, datetime
 
-OURA_TOKEN = "HNEMOKAN3CO3BZ3BLNAKLUK67QRAQYZM"
+        OURA_TOKEN = "HNEMOKAN3CO3BZ3BLNAKLUK67QRAQYZM"
 
-dt_now = datetime.datetime.now(datetime.timezone.utc)
-dt_12h_ago = dt_now - datetime.timedelta(hours=12)
+        dt_now = datetime.datetime.now(datetime.timezone.utc)
+        dt_12h_ago = dt_now - datetime.timedelta(hours=12)
 
-url = 'https://api.ouraring.com/v2/usercollection/heartrate'
-params = {
-    'start_datetime': dt_12h_ago.isoformat(),
-    'end_datetime': dt_now.isoformat()
-}
-headers = {'Authorization': f'Bearer {OURA_TOKEN}'}
+        url = 'https://api.ouraring.com/v2/usercollection/heartrate'
+        params = {
+            'start_datetime': dt_12h_ago.isoformat(),
+            'end_datetime': dt_now.isoformat()
+        }
+        headers = {'Authorization': f'Bearer {OURA_TOKEN}'}
 
-response = requests.get(url, headers=headers, params=params)
-data = response.json()
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
 
-hrs = data.get('data', [])
+        hrs = data.get('data', [])
 
-if hrs:
-    latest = hrs[-1]
-    latest_data = {
-        'bpm': latest['bpm'],
-        'timestamp': latest['timestamp']
-    }
-else:
-    latest_data = {}
+        if hrs:
+            latest = hrs[-1]
+            latest_data = {
+                'bpm': latest['bpm'],
+                'timestamp': latest['timestamp']
+            }
+        else:
+            latest_data = {}
 
-with open('static/data.json', mode='w', encoding='utf-8') as f:
-    json.dump(latest_data, f, ensure_ascii=False)
+
+        # 読み込んだ心拍数と時間をJSONファイルに保存
+        with open('static/data.json', mode='w', encoding='utf-8') as f:
+            json.dump(latest_data, f, ensure_ascii=False)
+            
+        # 保存したJSONファイルをdataとして使える形にして保存
+        with open('static/data.json', mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+        
+
+
