2750ee6a6e660e7a6dac445f9d7a1f884008466b
noinn <saito.shin.o2@elms.hokudai.ac.jp>
2025-03-21 12:37:44 +0900

    'homeの画面とbaseの画面を整えました'

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    latest_bpm, latest_timestamp = get_oura_data.get_lastbpm() # latest_bpmとlatest_timestampを取得
+    return render_template('home.html', latest_bpm=latest_bpm, latest_timestamp=latest_timestamp)
+
+@app.route("/timeline")
+def timeline():
+    return render_template('timeline.html')
 
 @app.route("/profile")
 def profile():
@@ -17,5 +22,17 @@ def profile():
 def post():
     return render_template('post.html')
 
+@app.errorhandler(500)
+def system_error(error):
+    error_description = error.description
+    return render_template('system_error.html', 
+                           error_description=error_description), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
--- cron.py
+++ cron.py
@@ -1,7 +1,10 @@
+
+import requests, json, datetime
+
 class get_oura_data:
     
+    @staticmethod
     def get_lastbpm():
-        import requests, json, datetime
 
         OURA_TOKEN = "HNEMOKAN3CO3BZ3BLNAKLUK67QRAQYZM"
 
@@ -36,7 +39,12 @@ class get_oura_data:
             
         # 保存したJSONファイルをdataとして使える形にして保存
         with open('static/data.json', mode='r', encoding='utf-8') as f:
-            data = json.load(f)
+            bpm_data = json.load(f)
         
+        latest_bpm = bpm_data.get('bpm')
+        latest_timestamp = bpm_data.get('timestamp')
+        
+        return latest_bpm, latest_timestamp
+      
 
 
